2bba50b2d1af3326b73b6765353518fbfcd57eed
Marcos Porto Mariño <php@marcospor.to>
2026-01-07 21:29:22 +0100

    Initial code from laminas/laminas-diactoros with references to Psr

---
+++ src/Exception/SerializationException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Diactoros\Exception;
+
+use UnexpectedValueException;
+
+class SerializationException extends UnexpectedValueException
+{
+    public static function forInvalidRequestLine(): self
+    {
+        return new self('Invalid request line detected');
+    }
+
+    public static function forInvalidStatusLine(): self
+    {
+        return new self('No status line detected');
+    }
+}
---
+++ src/Exception/UploadedFileAlreadyMovedException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Diactoros\Exception;
+
+use RuntimeException;
+use Throwable;
+
+class UploadedFileAlreadyMovedException extends RuntimeException
+{
+    /** {@inheritDoc} */
+    public function __construct(
+        string $message = 'Cannot retrieve stream after it has already moved',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
