5d83232b4538b514cd62f091253b46fb0a6b99e7
Marcos Porto Mariño <php@marcospor.to>
2026-01-09 00:28:07 +0100

    Refactor StreamInterface

--- src/CallbackStream.php
+++ src/CallbackStream.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * This file is part of the Rodas\Diactoros
+ *
+ * Based on Laminas\Diactoros\CallbackStream.php
+ * laminas/laminas-diactoros (Laminas\Diactoros) from Laminas Project a Series of LF Projects, LLC.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Rodas\Diactoros
+ * @copyright 2026 Marcos Porto <php@marcospor.to>
+ * @license https://opensource.org/license/mit The MIT License
+ * @link https://marcospor.to/repositories/diactoros
+ */
 
 declare(strict_types=1);
 
@@ -6,7 +20,7 @@ namespace Rodas\Diactoros;
 
 use InvalidArgumentException;
 use Override;
-use Psr\Http\Message\StreamInterface;
+use Rodas\Psr\Http\Message\StreamInterface;
 use Stringable;
 
 use function array_key_exists;
@@ -65,9 +79,8 @@ class CallbackStream implements StreamInterface, Stringable {
     /**
      * {@inheritdoc}
      */
-    #[Override]
-    public function getSize(): ?int {
-        return null;
+    public ?int $size {
+        get => null;
     }
 
     /**
@@ -89,9 +102,8 @@ class CallbackStream implements StreamInterface, Stringable {
     /**
      * {@inheritdoc}
      */
-    #[Override]
-    public function isSeekable(): bool {
-        return false;
+    public bool $isSeekable {
+        get => false;
     }
 
     /**
@@ -113,9 +125,8 @@ class CallbackStream implements StreamInterface, Stringable {
     /**
      * {@inheritdoc}
      */
-    #[Override]
-    public function isWritable(): bool {
-        return false;
+    public bool $isWritable {
+        get => false;
     }
 
     /**
@@ -129,9 +140,8 @@ class CallbackStream implements StreamInterface, Stringable {
     /**
      * {@inheritdoc}
      */
-    #[Override]
-    public function isReadable(): bool {
-        return false;
+    public bool $isReadable {
+        get => false;
     }
 
     /**
